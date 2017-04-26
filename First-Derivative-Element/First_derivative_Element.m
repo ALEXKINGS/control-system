7ccf2d4926bc778930fcbfd271ca782d0cee6e1b
刘传佳 <liuchuanjiacl@gmail.com>
2017-04-26 09:42:39 +0800

    First_derivative_Element
    learn control system ,add First_derivative_Element nyquist and bode
    diagrams

--- First-Derivative-Element/First_derivative_Element.m
+++ First-Derivative-Element/First_derivative_Element.m
@@ -1,11 +1,11 @@
 %/*************************************************************************************
-% *  File Name                                :   Oscillational_Element.m
+% *  File Name                                :   First_derivativel_Element.m
 % 
 % *  Create Date                             :    2017/04/25
 % *  Author/Corporation                :   Twsa
 % *  Description                              :   mechanical control engineering:
 %                                                         The frequency characteristics of typical 
-%                                                         Bode Diagram And Nyquist  Diagram of Oscillational_Element  
+%                                                         Bode Diagram And Nyquist  Diagram of First_derivativel_Element  
 
 % *  Return Code                           :    None
 % 
@@ -20,27 +20,28 @@
 % **************************************************************************************
 
 %Set struct params of system
-zeta=[0.1,0.2,0.3,0.4,0.5,0.7,1];
-% T=[10,100,1000];                               %set constant param of intertial element
-T=1;
+%zeta=[0.1,0.2,0.3,0.4,0.5,0.7,1];
+T=[10,100,1000];                               %set constant param of intertial element
+% T=1;
 
 %calculate every params of open loop about this system
-K=1;
-A1=T*T;
-A2=2*zeta*T;
+K1=T;
+K2=1;
+A1=0;
+A2=0;
 A3=1;
 %build the transfer function of open loop
-nG1=[K];                        %transfer function's numerator
-len=size(zeta);
-den_len=3;
-dG1=[zeros(len(2),den_len)];   %len(2):num of transfer function need to sim
+len=size(T);
+den_len=2;
+nG1=zeros(len(2),den_len)            %transfer function's numerator
+dG1=[A1,A2,A3];   
 
 % transfer module Initialize
-G1=[zeros(1,len(2))];
+G1=[zeros(1,len(2))]
 
 for t=1:1:len(2)
-    dG1(t,:)=[A1,A2(t),A3];     %transfer function's thedenominator
-    G1=tf(nG1,dG1(t,:));    %make transfer module
+    nG1(t,:)=[K1(t),K2]       %transfer function's thedenominator
+    G1=tf(nG1(t,:),dG1);    %make transfer module
 end
 
 %from the operations ,I can find the transfer function is Intergral-Element
@@ -49,7 +50,7 @@ end
 %Set Bode Diagram params
 %at this time,I have to draw the transfer function nyquist diagram
 figure(1); 
-title('Nyquist Diagram:Oscillational element');
+title('Nyquist Diagram:First_derivativel element');
 xlabel('real part ');
 ylabel('imaginary part');
 %Complete Setting
@@ -58,7 +59,7 @@ for t=1:1:len(2)
     axis([-3,5,-5,3]);                                    %sets scaling for the x- and y-axeson the current plot
     grid on;
     %function I use:nyquist(num,den)£»
-    [re1,im1]=nyquist(nG1,dG1(t,:));
+    [re1,im1]=nyquist(nG1(t,:),dG1);
     switch(t)
         case 1,
             plot(re1,im1,'b','LineWidth',1);                    %paint nyquist
@@ -69,35 +70,23 @@ for t=1:1:len(2)
         case 3,
             plot(re1,im1,'r','LineWidth',1);              %paint nyquist
             hold on
-        case 4,
-            plot(re1,im1,'c','LineWidth',1);              %paint nyquist
-            hold on
-        case 5,
-            plot(re1,im1,'m','LineWidth',1);              %paint nyquist
-            hold on
-        case 6,
-            plot(re1,im1,'y','LineWidth',1);              %paint nyquist
-            hold on
-        case 7,
-            plot(re1,im1,'k','LineWidth',1);              %paint nyquist
-            hold on
         otherwise,
             disp('The value is out of range. ');
     end
 end
-legend('zeta=0.1','zeta=0.2','zeta=0.3','zeta=0.4','zeta=0.5','zeta=0.7','zeta=1');
+legend('T=10','T=100','T=1000');
 hold off;
 
 %Set Bode Diagram params
-%Add Bode diagram to Oscillational-Element
+%Add Bode diagram to First_derivativel-Element
 w=logspace(-5,3,1000);                        %in the space of logarithm,define frequency w
 figure(2);
 %Complete Setting
 
 for t=1:1:len(2)
     grid on;
-    bode(nG1,dG1(t,:),w);                       %paint Bode diagram
-    legend('zeta=0.1','zeta=0.2','zeta=0.3','zeta=0.4','zeta=0.5','zeta=0.7','zeta=1');
+    bode(nG1(t,:),dG1,w);                       %paint Bode diagram
+    legend('T=10','T=100','T=1000');
     hold on;
 end
 hold off;
